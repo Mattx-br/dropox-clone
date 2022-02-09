26b8d00794db9091716abba92b6e1fe03026b3e8
Mattx-br <anthonytx22nunes@gmail.com>
2022-02-09 17:31:43 -0300

    :heavy_plus_sign: Adding DropBox controller and Creating an event click on 'Send Files' button

---
+++ app/public/src/controller/DropBoxController.js
@@ -0,0 +1,23 @@
+class DropBoxController {
+    constructor() {
+        this.btnSendFileEl = document.querySelector('#btn-send-file');
+        this.inputFilesEl = document.querySelector('#files');
+        this.snakeModalEl = document.querySelector('#react-snackbar-root');
+        this.initEvents();
+    }
+
+    initEvents() {
+        this.btnSendFileEl.addEventListener('click', event => {
+
+            this.inputFilesEl.click();
+
+        });
+
+        this.inputFilesEl.addEventListener('change', event => {
+
+            console.log(event.target.files);
+
+            this.snakeModalEl.style.display = 'block';
+        })
+    }
+}
