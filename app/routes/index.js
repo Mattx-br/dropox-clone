04c884506cc216b6d4b79bb1c6495acf76574ace
Mattx-br <anthonytx22nunes@gmail.com>
2022-02-11 14:34:45 -0300

    :triangular_flag_on_post: Create path to open files

--- app/routes/index.js
+++ app/routes/index.js
@@ -5,48 +5,71 @@ var fs = require('fs')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Express' });
+});
+
+router.get('/file', (req, res) => {
+
+    let path = './' + req.query.path;
+
+    if (fs.existsSync(path)) {
+
+        fs.readFile(path, (err, data) => {
+
+            if (err) {
+                console.error(err);
+                res.status(400).json({ error: err });
+            } else {
+                res.status(200).end(data);
+            }
+        })
+    } else {
+        res.status(404).json({ error: 'File not found' });
+    }
+
 });
 
 router.delete('/file', (req, res) => {
-  let form = new formidable.IncomingForm({
-    uploadDir: './upload',
-    keepExtensions: true
-  })
+    let form = new formidable.IncomingForm({
+        uploadDir: './upload',
+        keepExtensions: true
+    })
 
-  form.parse(req, (err, fields, files) => {
+    form.parse(req, (err, fields, files) => {
 
-    let path = './' + fields.path;
+        let path = './' + fields.path;
 
-    if (fs.existsSync(path)) {
-      fs.unlink(path, err => {
-        if (err) {
-          res.status(400).json({
-            err
-          })
+        if (fs.existsSync(path)) {
+            fs.unlink(path, err => {
+                if (err) {
+                    res.status(400).json({
+                        err
+                    })
+                } else {
+                    res.json({
+                        fields
+                    })
+                }
+            })
         } else {
-          res.json({
-            fields
-          })
+            res.status(404).json({ error: 'File not found' });
         }
-      })
-    }
-    
-  })
+
+    })
 })
 
 router.post('/upload', (req, res) => {
 
-  let form = new formidable.IncomingForm({
-    uploadDir: './upload',
-    keepExtensions: true
-  })
-
-  form.parse(req, (err, fields, files) => {
-    res.json({
-      files
+    let form = new formidable.IncomingForm({
+        uploadDir: './upload',
+        keepExtensions: true
     })
-  })
+
+    form.parse(req, (err, fields, files) => {
+        res.json({
+            files
+        })
+    })
 
 })
 
